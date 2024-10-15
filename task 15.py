347034a4bc32fce7c7ea2f866642edf4af14f290
AronBobby12022006 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2024-10-15 16:05:00 +0530

    Add files via upload

---
+++ task 15.py
@@ -0,0 +1,12 @@
+"""
+Author:Aron Bobby Daniel
+Date:14-10-2024
+Python programme to generate the  odd numbers
+"""
+limit=int(input("Enter number:"))
+odd_number=1
+count=0
+while count<limit:
+      print(odd_number,"\t",end="")
+      count+=1
+      odd_number+=2
